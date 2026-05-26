b226a58079a80aa34ba73081313020ed023d865e
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2026-05-26 13:26:05 +0000

    Updated the generation script.

--- sh/ghGenerate.sh
+++ sh/ghGenerate.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-COMPRESS_CRIT="\.(bin|bm|bmp|conf|css|csv|htm|html|ico|js|json|kar|list|lst|map|md|mid|mjs|mod|otf|svg|tsv|ttf|vgm|wasm|webmanifest|xml)$"
+COMPRESS_CRIT="\.(ass|atom|bin|bm|bmp|conf|css|csv|htm|html|ico|js|json|kar|list|lrc|lst|map|md|mid|mjs|mod|mts|otf|rss|sbv|srt|ssa|svg|trc|ts|tsv|ttf|ttml|txt|vgm|vtt|wasm|webmanifest|xml|ytt)$"
 rm -v 'book/FontAwesome/fonts/fontawesome-webfont.eot' 'book/FontAwesome/fonts/fontawesome-webfont.ttf'
 echo "$(date +"%s")" > build-time.txt
 cp -r book book-gz
 cp -r book book-br
+cp -r book book-base
 cd book
 tar cvf ../pages-build.tar *
 cd ..
-zopfli --i1 -v pages-build.tar
+#zopfli --i1 -v pages-build.tar
+gzip -9v pages-build.tar
 rm -v pages-build.tar
 cd book-gz
 tree -ifl | while IFS= read -r file; do
@@ -40,5 +42,18 @@ tree -ifl | while IFS= read -r file; do
 	fi
 done
 tar cvf ../pages-build-br.tar *
+cd ..
+cd book-base
+tree -ifl | while IFS= read -r file; do
+	if [ -f "$file" ]; then
+		# Is a file
+		if [ "$(echo "$file" | grep -E "$COMPRESS_CRIT")" != "" ]; then
+			rm -v "$file"
+		else
+			echo "File \"${file}\" is preserved."
+		fi
+	fi
+done
+tar cvf ../pages-build-base.tar *
 cd ..
 exit
